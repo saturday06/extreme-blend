134bf4bd2bfb586aafaf6d2da592fae8c33f9bcd
Isamu Mogi <isamu@leafytree.jp>
2019-04-18 22:34:42 +0900

    init

---
+++ linux/main.cpp
@@ -0,0 +1,18 @@
+#include <assert.h>
+#include <cstdio>
+#include <cstdlib>
+#include <error.h>
+#include <wayland-server.h>
+
+int main(int argc, char *argv[])
+{
+  wl_display* display = wl_display_create();
+
+  const char* socket_name = wl_display_add_socket_auto(display);
+  assert(socket_name);
+
+  printf("hello, wayland\n");
+  wl_display_run(display);
+
+  return 0;
+}
