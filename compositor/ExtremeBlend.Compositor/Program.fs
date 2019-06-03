43924c9697a30ccc73539968563ac1531117e813
Isamu Mogi <isamu@leafytree.jp>
2019-06-03 23:33:21 +0900

    wip

--- compositor/ExtremeBlend.Compositor/Program.fs
+++ compositor/ExtremeBlend.Compositor/Program.fs
@@ -1,12 +1,8 @@
-module ExtremeBlend.Vision
+module ExtremeBlend.Compositor
 
 open System
-open System.Threading.Tasks
-open Grpc.Core
-open ExtremeBlend.Proto
 
 [<EntryPoint>]
 let main argv =
-    let channel = Channel("127.0.0.1:50051", ChannelCredentials.Insecure)
     printfn "Hello World from F#!"
     0 // return an integer exit code
