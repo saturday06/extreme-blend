b48f4e69101040b454b8a146db7159a3614f92f2
Isamu Mogi <isamu@leafytree.jp>
2019-08-06 05:32:53 +0900

    wip

--- reflex/src/uds.rs
+++ reflex/src/uds.rs
@@ -5,30 +5,41 @@ use std::io;
 use std::thread;
 use std::time::{Instant, Duration};
 use std::ops::Add;
+use futures::future::Future;
+use futures::future::lazy;
+use std::net::Shutdown;
+use std::net::SocketAddr;
+use tokio::net::TcpStream;
+use tokio::prelude::*;
+
+
 
 pub struct Deadline {
     when: Instant,
-    registration: Registration,
+    mio_registration: mio::Registration,
 }
 
 impl Deadline {
     pub fn new(when: Instant) -> Deadline {
         let (registration, set_readiness) = Registration::new2();
+        set_readiness.set_readiness(Ready::empty());
 
         thread::spawn(move || {
+            println!("waiting");
             let now = Instant::now();
 
             if now < when {
                 thread::sleep(when - now);
             }
 
+            println!("set readiness");
             set_readiness.set_readiness(Ready::readable());
         });
-
-        Deadline {
+        let d = Deadline {
             when: when,
-            registration: registration,
-        }
+            mio_registration: registration,
+        };
+        d
     }
 
     pub fn is_elapsed(&self) -> bool {
@@ -44,7 +55,7 @@ impl Evented for Deadline {
         interest: Ready,
         opts: PollOpt,
     ) -> io::Result<()> {
-        self.registration.register(poll, token, interest, opts)
+        self.mio_registration.register(poll, token, interest, opts)
     }
 
     fn reregister(
@@ -54,22 +65,76 @@ impl Evented for Deadline {
         interest: Ready,
         opts: PollOpt,
     ) -> io::Result<()> {
-        self.registration.reregister(poll, token, interest, opts)
+        self.mio_registration.reregister(poll, token, interest, opts)
     }
 
     fn deregister(&self, poll: &Poll) -> io::Result<()> {
-        self.registration.deregister(poll)
+        self.mio_registration.deregister(poll)
+    }
+}
+
+struct Deadline2 {
+    tokio_registration: tokio::reactor::Registration,
+    deadline: Deadline,
+}
+
+impl Deadline2 {
+    fn new() -> Deadline2 {
+        let d = Deadline::new(Instant::now().add(Duration::from_millis(5000)));
+        let r = tokio::reactor::Registration::new();
+        r.register(&d);
+
+        Deadline2 {
+            deadline: d,
+            tokio_registration: r,
+        }
+    }
+}
+
+impl Future for Deadline2 {
+    type Item = ();
+    type Error = ();
+
+    fn poll(&mut self) -> Result<Async<()>, ()> {
+        match self.tokio_registration.poll_read_ready() {
+            Ok(Async::Ready(socket)) => {
+               // println!("peer address = {}", socket.peer_addr().unwrap());
+                Ok(Async::Ready(()))
+            }
+            Ok(Async::NotReady) => Ok(Async::NotReady),
+            Err(e) => {
+                println!("failed to connect: {}", e);
+                Ok(Async::Ready(()))
+            }
+        }
     }
 }
 
 #[test]
 fn it_works() {
-    let mut reg = tokio::reactor::Registration::new();
-    let mut dea = Deadline::new(Instant::now().add(Duration::from_millis(5000)));
-    reg.register(&dea);
+    let mut dea = Deadline2::new();
 
+    println!("start");
+    /*
+    tokio::run(lazy(|| {
+        panic!("oops");
 
-    assert!(true);
-
-
+        for i in 0..4 {
+            tokio::spawn(lazy(move || {
+                eprintln!("Hello from task {}", i);
+                Ok(())
+            }));
+        }
+        Ok(())
+    }));
+    tokio::run(futures::future::ok(reg)
+        .map(|r| {println!("ok1"); r})
+        .and_then(|r| {println!("ok2"); Ok(r)})
+        .and_then(|r| r.poll_read_ready())
+        .map(|_| println!("ok3"))
+        .map_err(|_: std::io::Error| ()));
+*/
+    tokio::run(dea);
+    println!("end");
+    assert!(false);
 }
